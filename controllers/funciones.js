8d98b66bde869e73f31be6a6151ae5cb2f95f681
DaromGamer <105200449+DaromGamer@users.noreply.github.com>
2022-05-20 17:02:39 -0400

    Update funciones.js

--- controllers/funciones.js
+++ controllers/funciones.js
@@ -3,7 +3,7 @@ const conexion = require('../database/db')
 const {promisify} = require('util')
 
 function stockf() {
-     const probando
+     probando = 0
      conexion.query ('SELECT * FROM Vehiculos WHERE Modelo = ?', ["ford mustang"], (error, results)=>{
         console.log(results[0].Stock)
         probando = results[0].Stock
