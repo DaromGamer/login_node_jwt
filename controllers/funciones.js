9733e9fa9745e3f532ae4a5122ab086a9ea25090
DaromGamer <105200449+DaromGamer@users.noreply.github.com>
2022-05-24 15:14:18 -0400

    Update funciones.js

--- controllers/funciones.js
+++ controllers/funciones.js
@@ -6,22 +6,26 @@ function stockf() {
      probando = 0
      conexion.query ('SELECT * FROM Vehiculos WHERE Modelo = ?', ["ford mustang"], async (error, results)=>{
         console.log(results[0].Stock)
-        const probando = results[0].Stock
+        probando = results[0].Stock
         return probando
         
     })
      return probando
 }
 function stockf2() {
-     const probando = 1
+     probando2 = 1
      conexion.query ('SELECT * FROM Vehiculos WHERE Modelo = ?', ["ford mustang"],(prepare: true) {
     if (err) throw err;
     probando2 = row[0].Stock
-    return probando
+    return probando2
      })
+     return probando2
 }
                      //SELECT u.Mail, u.Nombre FROM Vendedor as v, Usuario as u WHERE v.Mail = "ripazha.darom@gmail.com" and u.Mail = "ripazha.darom@gmail.com" esto es
                      //para lo del autentic
+//resultset buscar
+//const query = 'SELECT name, email, address FROM users WHERE id = ?';
+//const result = await client.execute(query, [ id ], { prepare: true });
 
 
 module.exports.elstock = stockf2
