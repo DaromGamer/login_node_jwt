edf40e9d97e25f1e961f6b4518c392ded340384b
DaromGamer <105200449+DaromGamer@users.noreply.github.com>
2022-05-24 14:56:57 -0400

    Update funciones.js

--- controllers/funciones.js
+++ controllers/funciones.js
@@ -12,5 +12,16 @@ function stockf() {
     })
      return probando
 }
+function stockf2() {
+     const probando = 1
+     conexion.query ('SELECT * FROM Vehiculos WHERE Modelo = ?', ["ford mustang"],function(err, rows, fields) {
+    if (err) throw err;
 
-module.exports.elstock = stockf
+    for (var i = 0; i < rows.length; i++) {
+          probando2 = row[i][0].Stock
+    }
+    return probando
+}
+
+
+module.exports.elstock = stockf2
