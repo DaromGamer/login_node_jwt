9e3af0255b75c3bb1750a774ae484ef82709e30a
DaromGamer <105200449+DaromGamer@users.noreply.github.com>
2022-05-26 04:06:22 -0400

    a

--- controllers/funciones.js
+++ controllers/funciones.js
@@ -12,11 +12,35 @@ function stockf() {
     })
      return probando
 }
-function stockf2() {
+/*function stockf2() {
      probando2 = 1
      //probando2 = await conexion.query ('SELECT * FROM Vehiculos WHERE Modelo = ?', ["ford mustang"],{prepare: true})
      return probando2
-}
+}*/
+function main() {
+     const doQuery = (query) => {
+         return new Promise((resolve, reject) => {
+             con.query(query, (error, results, fields) => {
+                 if(error) return reject(error);
+                 console.log('Consulta correcta');
+                 return resolve(results);
+             });
+         });
+     }
+ 
+     // si deseo utilizar el resultado de la consulta,
+     // debo crear una función asíncrona y llamar a doQuery() usando await.
+     const doStuffWithResults = async () => {
+         const selectAllQuery = 'SELECT * FROM Vehiculos WHERE Modelo = "ford mustang"';
+         const results = await doQuery(selectAllQuery);
+         console.log(results);
+         // Aquí puedes usar el resultado de tu consulta
+     }
+ 
+     // llamamos a nuestro método
+     return doStuffWithResults();
+ }
+
                      //SELECT u.Mail, u.Nombre FROM Vendedor as v, Usuario as u WHERE v.Mail = "ripazha.darom@gmail.com" and u.Mail = "ripazha.darom@gmail.com" esto es
                      //para lo del autentic
 //resultset buscar
@@ -24,4 +48,4 @@ function stockf2() {
 //const result = await client.execute(query, [ id ], { prepare: true });
 
 
-module.exports.elstock = stockf2
+module.exports.elstock = main
